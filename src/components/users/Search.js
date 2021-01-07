7bb0c4eece53c117d0f959d75107e12ed8fb6ef6
AlexsandarM <59208791+AlexsandarM@users.noreply.github.com>
2021-01-07 15:45:37 +0100

    Section 4: 21: Routing in React

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,13 +1,61 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
+  state = {
+    text: '',
+  };
+
+  static propTypes = {
+    setSearch: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    if (this.state.text === '') {
+      this.props.setAlert('Please enter a user!', 'light');
+    } else {
+      this.props.setSearch(this.state.text);
+      this.setState({ text: '' });
+    }
+  };
+
   render() {
     return (
       <div>
-        
+        <form onSubmit={this.onSubmit} className='form'>
+          <input
+            type='text'
+            name='text'
+            id=''
+            placeholder='Search Users...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
+          <input
+            type='submit'
+            className='btn btn-dark btn-block'
+            value='Search'
+          />
+        </form>
+        {this.props.showClear && (
+          <button
+            className='btn btn-light btn-block'
+            onClick={this.props.clearUsers}
+          >
+            Clear
+          </button>
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default Search
+export default Search;
